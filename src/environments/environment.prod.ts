49658a6eadd37a35769e41bec7075902a798dc5a
jeetadhikari92 <jeet.adhikari92@gmail.com>
2018-11-19 03:15:04 +0530

    Initial commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCSnEP3mq8fK_aGUB5xg12LPJEMrGzNPM4",
+    authDomain: "ng-fitness-tracker-92b15.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-92b15.firebaseio.com",
+    projectId: "ng-fitness-tracker-92b15",
+    storageBucket: "ng-fitness-tracker-92b15.appspot.com",
+    messagingSenderId: "162991468203"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCSnEP3mq8fK_aGUB5xg12LPJEMrGzNPM4",
+    authDomain: "ng-fitness-tracker-92b15.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-92b15.firebaseio.com",
+    projectId: "ng-fitness-tracker-92b15",
+    storageBucket: "ng-fitness-tracker-92b15.appspot.com",
+    messagingSenderId: "162991468203"
+  }
+};
